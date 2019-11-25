aa7cbfc21b872878f68092a4ad31804900413f11
kitano <kitano.prog@gmail.com>
2019-11-25 17:56:40 +0900

    Initial Commit

---
+++ Rooms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rooms
+//
+//  Created by kitano on 2019/11/25.
+//  Copyright © 2019 ktnydi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
